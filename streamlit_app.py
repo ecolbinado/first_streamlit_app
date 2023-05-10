c3dcca4629324da47a800f7402f79382d2ad2422
Emil Olbinado <olbinado11@yahoo.com>
2023-05-10 20:44:51 +0800

    Update streamlit_app.py
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import requests
 import streamlit
 
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -29,6 +30,8 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
